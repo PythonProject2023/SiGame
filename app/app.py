b21d86a598f1c0c90017078c1dbe788cfe233e08
IgorYashch <yashch.igor@gmail.com>
2023-06-12 19:12:13 +0300

    Добавил шаблон для страницы присоединения к игре

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-import random
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -12,29 +11,33 @@ from kivy.uix.slider import Slider
 class MainMenu(Screen):
     def __init__(self, **kwargs):
         super(MainMenu, self).__init__(**kwargs)
-        self.layout = BoxLayout(orientation='vertical')
-        self.layout.add_widget(Label(text='Своя игра', font_size=40))
+        self.layout = BoxLayout(orientation="vertical")
+        self.layout.add_widget(Label(text="Своя игра", font_size=40))
 
         self.buttons = [
-            ('Создать игру', 'create_game'),
-            ('Присоединиться к игре', 'join_game'),
-            ('Правила', 'rules'),
-            ('Выход', 'exit'),
+            ("Создать игру", "create_game"),
+            ("Присоединиться к игре", "join_game"),
+            ("Правила", "rules"),
+            ("Выход", "exit"),
         ]
 
         for text, screen_name in self.buttons:
-            button = Button(text=text, size_hint=(1, 0.2), on_release=self.switch_to_screen(screen_name))
+            button = Button(
+                text=text,
+                size_hint=(1, 0.2),
+                on_release=self.switch_to_screen(screen_name),
+            )
             self.layout.add_widget(button)
 
         self.add_widget(self.layout)
 
     def switch_to_screen(self, screen_name):
         def switch(*args):
-            if screen_name == 'exit':
+            if screen_name == "exit":
                 App.get_running_app().stop()
             else:
                 self.manager.current = screen_name
-                
+
         return switch
 
 
@@ -43,25 +46,27 @@ class CreateGame(Screen):
         super(CreateGame, self).__init__(**kwargs)
         self.layout = GridLayout(cols=2, padding=10, spacing=10)
 
-        self.layout.add_widget(Label(text='Название игры:', font_size=20))
+        self.layout.add_widget(Label(text="Название игры:", font_size=20))
         self.game_name = TextInput(multiline=False)
         self.layout.add_widget(self.game_name)
 
-        self.layout.add_widget(Label(text='Пароль:', font_size=20))
+        self.layout.add_widget(Label(text="Пароль:", font_size=20))
         self.password = TextInput(multiline=False, password=True)
         self.layout.add_widget(self.password)
 
-        self.layout.add_widget(Label(text='Количество игроков:', font_size=20))
+        self.layout.add_widget(Label(text="Количество игроков:", font_size=20))
         self.players_slider = Slider(min=2, max=5, value=2, step=1)
         self.layout.add_widget(self.players_slider)
 
-        self.layout.add_widget(Label(text='Прикрепить пакет:', font_size=20))
+        self.layout.add_widget(Label(text="Прикрепить пакет:", font_size=20))
         self.package_path = TextInput(multiline=False)
         self.layout.add_widget(self.package_path)
 
-        self.create_room_button = Button(text='Создать комнату', on_release=self.create_room)
+        self.create_room_button = Button(
+            text="Создать комнату", on_release=self.create_room
+        )
         self.layout.add_widget(self.create_room_button)
-        self.layout.add_widget(Label())  
+        self.layout.add_widget(Label())
 
         self.add_widget(self.layout)
 
@@ -79,10 +84,47 @@ class CreateGame(Screen):
         print(f"Путь к пакету: {package_path}")
 
         # Переход на экран игры после создания комнаты
-        self.manager.current = 'game'     
-        
-        
+        self.manager.current = "game"
+
+
 class JoinGame(Screen):
+    def __init__(self, **kwargs):
+        super(JoinGame, self).__init__(**kwargs)
+        self.layout = GridLayout(cols=2, padding=10, spacing=10)
+
+        self.layout.add_widget(Label(text="Название игры:", font_size=20))
+        self.game_name = TextInput(multiline=False)
+        self.layout.add_widget(self.game_name)
+
+        self.layout.add_widget(Label(text="Пароль:", font_size=20))
+        self.password = TextInput(multiline=False, password=True)
+        self.layout.add_widget(self.password)
+
+        self.layout.add_widget(Label(text="Ваше имя:", font_size=20))
+        self.player_name = TextInput(multiline=False)
+        self.layout.add_widget(self.player_name)
+
+        self.join_button = Button(text="Присоединиться", on_release=self.join_game)
+        self.layout.add_widget(self.join_button)
+        self.layout.add_widget(Label())
+
+        self.add_widget(self.layout)
+
+    def join_game(self, *args):
+        game_name = self.game_name.text
+        password = self.password.text
+        player_name = self.player_name.text
+
+        print("Присоединение к игре")
+        print(f"Название игры: {game_name}")
+        print(f"Пароль: {password}")
+        print(f"Ваше имя: {player_name}")
+
+        # Переход на экран игры после присоединения к комнате
+        self.manager.current = "game"
+
+
+class Game(Screen):
     pass
 
 
@@ -93,9 +135,10 @@ class Rules(Screen):
 class MyApp(App):
     def build(self):
         screen_manager = ScreenManager()
-        screen_manager.add_widget(MainMenu(name='main_menu'))
-        screen_manager.add_widget(CreateGame(name='create_game'))
-        screen_manager.add_widget(JoinGame(name='join_game'))
-        screen_manager.add_widget(Rules(name='rules'))
+        screen_manager.add_widget(MainMenu(name="main_menu"))
+        screen_manager.add_widget(CreateGame(name="create_game"))
+        screen_manager.add_widget(JoinGame(name="join_game"))
+        screen_manager.add_widget(Rules(name="rules"))
+        screen_manager.add_widget(Game(name="game"))
 
         return screen_manager
