940db74ddeea9b62b90ae300546dfcede8bf6a69
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2023-06-12 19:01:41 +0300

    Добавлена обработка ответа

--- parser.py
+++ parser.py
@@ -4,6 +4,34 @@ import sys
 import xml.etree.ElementTree as ET
 
 
+class Answer:
+    def __init__(self, right, wrong=None, text=None, image=None, sound=None, video=None):
+        self.right = right
+        self.wrong = wrong
+        self.text = text
+        self.image = image
+        self.sound = sound
+        self.video = video
+
+    def get_right(self):
+        return self.right
+
+    def get_wrong(self):
+        return self.wrong
+    
+    def get_text(self):
+        return self.text
+
+    def get_image(self):
+        return self.image
+
+    def get_sound(self):
+        return self.sound
+
+    def get_video(self):
+        return self.video
+
+
 class Question:
 
     def __init__(self, price, text=None, image=None, sound=None, video=None):
@@ -27,6 +55,12 @@ class Question:
 
     def get_video(self):
         return self.video
+
+    def add_answer(self, ans):
+        self.ans = ans
+
+    def get_answer(self):
+        return self.ans
 
 
 class Theme:
@@ -78,7 +112,7 @@ class Package:
         yield from self.rounds
 
     def __str__(self):
-        return f"{author}\n" + '\n'.join(self.rounds)
+        return f"{author}\n\n" + '\n'.join(str(i) for i in self.rounds)
 
 
 directory_path = Path.cwd()
@@ -98,8 +132,8 @@ p = Package()
 # Используем пространство имен при поиске элементов
 info = root.find('ns:info', namespace)
 authors = info.find('ns:authors', namespace)
-author = authors.find('ns:author', namespace)
-p.set_author(author.text)
+author = authors.find('ns:author', namespace).text
+p.set_author(author)
 
 game = root.find('ns:rounds', namespace)
 for rnd in game.findall('ns:round', namespace):
@@ -117,7 +151,9 @@ for rnd in game.findall('ns:round', namespace):
             snd = None
             vd = None
             scen = q.find('ns:scenario', namespace)
-            for atom in scen.findall('ns:atom', namespace):
+            marker_flag = False
+            it = iter(scen.findall('ns:atom', namespace))
+            for atom in it:
                 if (tp := atom.get('type')):
                     match tp:
                         case 'image':
@@ -126,9 +162,36 @@ for rnd in game.findall('ns:round', namespace):
                             snd = atom.text
                         case 'video':
                             vd = atom.text
-                        case 'marker':   # всё, что дальше - ответ
-                            continue
+                        case 'marker':   #всё, что дальше - ответ
+                            marker_flag = True
+                            break
                         case _:
                             txt = atom.text
             Q = Question(pr, txt, im, snd, vd)
+            if marker_flag:
+                r_ans = None
+                w_ans = None
+                txt = None
+                im = None
+                snd = None
+                vd = None
+                for atom in it:
+                    if (tp := atom.get('type')):
+                        match tp:
+                            case 'image':
+                                im = atom.text
+                            case 'voice':
+                                snd = atom.text
+                            case 'video':
+                                vd = atom.text
+                            case _:
+                                txt = atom.text
+            else:
+                r_ans = q.find('ns:right', namespace)
+                r_ans = r_ans.find('ns:answer', namespace).text
+                w_ans = q.find('ns:wrong', namespace)
+                if w_ans is not None:
+                    w_ans = w__ans.find('ns:answer', namespace).text  # надо найти пакет
+            ans = Answer(r_ans, w_ans, txt, im, snd, vd)
+            Q.add_answer(ans)
             T.add_question(Q)
